df32378dd8a850363edf5b13078fd5cef0a6248a
jon <joncolque@gmail.com>
2017-10-12 16:48:13 -0300

    circulo. Lista horizontal. Bootstrap y Jquery.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,10 +3,16 @@ import { Component } from "@angular/core"
 @Component(
     {
         selector:'home',
-        templateUrl:'./home.component.html'
+        templateUrl:'./home.component.html',
+        styleUrls:['../draws/draws.component.css']
     }
 )
 
 export class HomeComponent{
     titleHome = 'Bienvenido al Home'
+    areas:Array<string>=['I+D','Desarrollo','RRHH']
+
+    getProjects(){
+        console.log("no hay proyectos")
+    }
 }
